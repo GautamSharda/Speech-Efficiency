d381f57e25c898739205330cf43435c7583887a2
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2020-08-06 00:55:40 -0500

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -16,3 +16,8 @@ with sr.AudioFile(AUDIO_FILE) as source:
         audio = r.record(source)  # read the entire audio file                  
 
         print("Transcription: " + str(r.recognize_google(audio, language='en-US', show_all=True)))
+        
+#search for inefficiencies
+i = str(r.recognize_google(audio, language='en-US', show_all=True))
+if "our evidence indicates that" in i:
+        print("Instead of saying \"our evidence indicates that\", explain the argument the evidence is making first, and then follow it up with a citation")  
